948a3ec5adcc5b34d8de0332604f5401471ea654
NhlFrz <firoznihal@gmail.com>
2024-07-02 10:31:51 +0400

    add to cart change

--- backend/src/routes/RestaurantRoute.ts
+++ backend/src/routes/RestaurantRoute.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import Restaurant from "../models/restaurant";
+import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
+
+router.get("/:restaurantId", 
+param("city")
+.isString()
+.trim()
+.notEmpty()
+.withMessage("City parameter must be a valid string"),
+RestaurantController.getRestaurant
+);
 
 router.get(
     "/search/:city",
